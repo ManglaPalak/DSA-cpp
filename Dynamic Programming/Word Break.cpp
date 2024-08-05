31a417ade38d72827ca1f91d42230f839aa0bb32
ManglaPalak <122383012+ManglaPalak@users.noreply.github.com>
2024-08-05 12:27:53 +0530

    Create Word Break.cpp

---
+++ Dynamic Programming/Word Break.cpp
@@ -0,0 +1,18 @@
+bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.length()+1,false);
+        dp[0]=true;
+        unordered_set<string> wordset;
+        for(string i:wordDict){
+            wordset.insert(i);
+        }
+        for(int i=1;i<=s.length();i++){
+            for(string word:wordDict){
+                int start=i-word.length();
+                if (start>=0 && dp[start] && s.substr(start,word.length())==word){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
