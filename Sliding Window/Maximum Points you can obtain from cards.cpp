c38a26cedea70b19af9e909a987d5dbe9a094cd3
ManglaPalak <122383012+ManglaPalak@users.noreply.github.com>
2024-08-05 21:46:33 +0530

    Create Maximum Points you can obtain from cards.cpp

---
+++ Sliding Window/Maximum Points you can obtain from cards.cpp
@@ -0,0 +1,17 @@
+int maxScore(vector<int>& cardPoints, int k) {
+        int lsum=0;
+        int rsum=0;
+        int maxSum=0;
+        for(int i=0;i<k;i++){
+            lsum+=cardPoints[i];
+        }
+        maxSum=lsum;
+        int rindex=cardPoints.size()-1;
+        for(int i=k-1;i>=0;i--){
+            lsum=lsum-cardPoints[i];
+            rsum+=cardPoints[rindex];
+            rindex--;
+            maxSum=max(maxSum,lsum+rsum);
+        }
+        return maxSum;
+    }
