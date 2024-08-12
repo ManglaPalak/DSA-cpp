327831ded696040a6198ae2e7092e592e3a420f1
ManglaPalak <122383012+ManglaPalak@users.noreply.github.com>
2024-08-12 18:09:40 +0530

    Update Preorder Traversal in Binary Tree.cpp

--- Binary Tree/Preorder Traversal in Binary Tree.cpp
+++ Binary Tree/Preorder Traversal in Binary Tree.cpp
@@ -25,3 +25,25 @@ public:
         return ans;
     }
 };
+
+// ################################### iterarive preorder traversal ##########################################################
+vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans;
+        if (root==NULL){
+            return ans;
+        }
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* front=st.top();
+            st.pop();
+            ans.push_back(front->val);
+            if (front->right){
+                st.push(front->right);
+            }
+            if (front->left){
+                st.push(front->left);
+            }
+        }
+        return ans;
+    }
