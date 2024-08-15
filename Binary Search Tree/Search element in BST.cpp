ba25557412134c2e257158c3c54b846e731efe2e
ManglaPalak <122383012+ManglaPalak@users.noreply.github.com>
2024-08-15 18:27:11 +0530

    Create Search element in BST.cpp

---
+++ Binary Search Tree/Search element in BST.cpp
@@ -0,0 +1,14 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root==NULL){
+        return NULL;
+    }
+    if (root->val == val){
+        return root;
+    }
+    if (val<root->val){
+        return searchBST(root->left,val);
+    }
+    else{
+        return searchBST(root->right,val);
+    }
+}
